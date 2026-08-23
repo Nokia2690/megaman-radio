ed4bf2f333688f2a790d0367e2e26f5c72469110
JuanPonce333 <pipeponce334@gmail.com>
2026-08-22 19:56:31 -0500

    test

---
+++ player.js
@@ -0,0 +1,82 @@
+
+const titulo = document.querySelector('.reproductor-musica h1');
+const nombre = document.querySelector('.reproductor-musica p');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
+
+const iconoControl = document.getElementById('iconoControl');
+const botonReproducir = document.querySelector('.controles button.boton-iniciar-pausar');
+
+const botonAtras = document.querySelector('.controles button.atras');
+const botonAdelante = document.querySelector('.controles button.siguiente');
+
+let canciones = [];
+let indiceCancionActual = 0;
+
+async function cargarCanciones() {
+    const response = await fetch("./tracks.json");
+    canciones = await response.json();
+
+    actualizarCancion();
+}
+
+function actualizarCancion(){
+    titulo.textContent = canciones[indiceCancionActual].title;
+    nombre.textContent = canciones[indiceCancionActual].artist;
+    cancion.src = canciones[indiceCancionActual].src;
+    cancion.addEventListener('loadeddata', function(){});
+}
+
+botonReproducir.addEventListener('click', reproducirPausar);
+
+function reproducirPausar(){
+
+    if(cancion.paused){
+        reproducirCancion();
+    } else {
+        pausarCancion();
+    }
+}
+
+function reproducirCancion(){
+    cancion.play();
+    iconoControl.classList.add('bi-pause-fill');
+    iconoControl.classList.remove('bi-play-fill');    
+}
+
+function pausarCancion() {
+    cancion.pause();
+    iconoControl.classList.remove('bi-play-fill');
+    iconoControl.classList.add('bi-pause-fill');    
+}
+
+cancion.addEventListener('timeupdate', function(){
+    if(!cancion.paused){
+        progreso.value = cancion.currentTime;
+    }
+})
+
+progreso.addEventListener('input', function(){
+    cancion.currentTime = progreso.value;
+})
+
+botonAdelante.addEventListener('click', function(){
+    indiceCancionActual = (indiceCancionActual + 1) % canciones.length;
+    actualizarCancion();
+    reproducirCancion();
+})
+
+botonAtras.addEventListener('click', function(){
+    indiceCancionActual = (indiceCancionActual - 1) % canciones.length;
+    actualizarCancion();
+    reproducirCancion();
+})
+
+cancion.addEventListener('ended', function(){
+    indiceCancionActual = (indiceCancionActual + 1) % canciones.length;
+    actualizarCancion();
+    reproducirCancion();
+})
+
+cargarCanciones();
