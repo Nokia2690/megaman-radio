fa0f30061b673922515c5d980cff8c5fa98e2707
JuanPonce333 <pipeponce334@gmail.com>
2026-09-04 23:49:50 -0500

    volume linear, dynamic progress bar timestamp, zero and zx ost added

--- player.js
+++ player.js
@@ -1,4 +1,6 @@
 const R2_url = "https://pub-3d52a5c211294455ba197b8d4a6c5a1b.r2.dev/"
+const logDBRange = 60
+const log_a = 1 / 10 ** (logDBRange / 20);
 
 const titulo = document.querySelector('.reproductor-musica h1');
 const nombre = document.querySelector('.reproductor-musica p');
@@ -20,11 +22,13 @@ let indiceCancionActual = 0;
 async function cargarCanciones() {
     const response1 = await fetch("./tracks-classic.json");
     const response2 = await fetch("./tracks-x.json");
+    const response3 = await fetch("./tracks-zerozx.json");
 
     const classic = await response1.json();
     const x = await response2.json();
+    const zero = await response3.json();
 
-    canciones = [...classic, ...x];
+    canciones = [...classic, ...x, ...zero];
 
     actualizarCancion();
 }
@@ -60,14 +64,28 @@ function pausarCancion() {
 }
 
 cancion.addEventListener('timeupdate', function(){
-    if(!cancion.paused){
-        progreso.value = cancion.currentTime;
-    }
+    progreso.value = cancion.currentTime;
+    duracion.textContent = `${convertirTiempo(cancion.currentTime)} / ${convertirTiempo(cancion.duration)}`;
 })
 
 progreso.addEventListener('input', function(){
     cancion.currentTime = progreso.value;
 })
+
+cancion.addEventListener("loadedmetadata", function(){
+    progreso.max = cancion.duration;
+    duracion.textContent = `0:00 / ${convertirTiempo(cancion.duration)}`;
+})
+
+function convertirTiempo(segundos){
+    if(isNaN(segundos)){
+        return "0:00";
+    }
+    const minutos = Math.floor(segundos / 60);
+    const segundosRestantes = Math.floor(segundos % 60);
+
+    return `${minutos}: ${segundosRestantes.toString().padStart(2, "0")}`;
+}
 
 botonAdelante.addEventListener('click', function(){
     historialCanciones.push(indiceCancionActual);
@@ -90,9 +108,33 @@ cancion.addEventListener('ended', function(){
     reproducirCancion();
 })
 
-cancion.volume = volumeSlider.value / 100;
+let vol = 0.5;
+cancion.volume = vol;
+volumeSlider.value = vol * 100;
+
 volumeSlider.addEventListener('input', () => {
-    cancion.volume = volumeSlider.value / 100;
+    vol = Number(volumeSlider.value) / 100;
+    cancion.volume = vol;
 })
+
+function subirVolumen(){
+    if(vol <= 0){
+        vol = log_a;
+    } else {
+        vol *= 1.258925;
+        vol = Math.min(1.0, vol);
+    }
+    cancion.volume = vol;
+    volumeSlider.voule = vol * 100
+}
+
+function bajarVolumen(){
+    vol /= 1.258925;
+    if(vol < log_a){
+        vol = 0;
+    }
+    cancion.volume = vol;
+    volumeSlider.voule = vol * 100    
+}
 
 cargarCanciones();
